30a080a584299b5fccbf17bc87c96ec94963a5a6
Milo007 <maxuuumaxuuu@gmail.com>
2024-07-04 12:45:08 +0700

    New makmak

--- app/page.js
+++ app/page.js
@@ -56,8 +56,8 @@ export default function Home() {
           <img src="/img/F1.png" className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">Card title</h5>
-            <p className="card-text">This is a longer card with supporting text below as a
-               natural lead-in to additional content.</p>
+            <p className="card-text">This is a longer card with supporting text below as a 
+            natural lead-in to additional content. This content is a little bit longer.</p>
           </div>
         </div>
       </div>
